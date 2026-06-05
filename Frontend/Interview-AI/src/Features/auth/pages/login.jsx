5e884b0b36331896c12dccd45581c0ec5736b613
gauranshsahu <gauranshsahu5@gmail.com>
2026-06-05 22:17:33 +0530

    Login and Register Form

--- Frontend/Interview-AI/src/Features/auth/pages/login.jsx
+++ Frontend/Interview-AI/src/Features/auth/pages/login.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
+import "../auth.form.scss"
+import { useNavigate,Link } from 'react-router';
 
 const Login = () => {
+
+    const navigate = useNavigate()
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-       <main>
-        <div className="form-container">
-            <h1>Login</h1>
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
 
-            <form>
+                <form onSubmit={handleSubmit}>
 
-            <div className="input-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name='email' placeholder='Enter Email Address'/>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name='email' placeholder='Enter Email Address' />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name='password' placeholder='Enter Password' />
+                    </div>
+
+                    <button className='button primary-button'>Login</button>
+
+                </form>
+
+                <p>Don't have an account? <Link to={"/register"}>Register</Link> </p>
+
             </div>
-
-            <div className="input-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name='password' placeholder='Enter Password'/>
-            </div>
-
-            <button className='button primary-button'>Login</button>
-
-            </form>
-        </div>
-       </main>
+        </main>
     );
-}   
+}
 
 export default Login;
