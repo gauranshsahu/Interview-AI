7a67260c2782e8ecf3fe0d7a0eded2fffbe179a1
gauranshsahu <gauranshsahu5@gmail.com>
2026-07-08 00:42:54 +0530

    updated flow

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -17,6 +17,7 @@ const ai = new GoogleGenAI({
 // }
 
 const interviewReportSchema = z.object({
+    matchScore: z.number().describe("A score between 0 and 100 indicating how well the candidate's profile matches the job describe"),
     technicalQuestions: z.array(z.object({
         question: z.string().description("The technical question can be asked in the interview"),
         intention: z.string().description("The intention of interviewer behind asking this question"),
@@ -29,11 +30,17 @@ const interviewReportSchema = z.object({
     })).description("Behavioral questions that can be asked in the interview along with their intention and how to answer them"),
     skillGaps: z.array(z.object({
         skill: z.string().description("The skill which the candidate is lacking"),
-        severity: z.enum(["low","medium","high"]).description("The severity of this skill gap, i.e. low,medium and high")
-    }))
+        severity: z.enum(["low", "medium", "high"]).description("The severity of this skill gap, i.e. how important is this skill for the job and how much it can impact the candidate's chances")
+    })).description("List of skill gaps in the candidate's profile along with their severity"),
+    preparationPlan: z.array(z.object({
+        day: z.number().description("The day number in the preparation plan, starting from 1"),
+        focus: z.string().describe("The main focus of this day in the preparation plan, e.g. data structures, system design, mock interviews etc."),
+        tasks: z.array(z.string()).describe("List of tasks to be done on this day to follow the preparation plan, e.g. read a specific book or article, solve a set of problems, watch a video etc.")
+    })).describe("A day-wise preparation plan for the candidate to follow in order to prepare for the interview effectively"),
+    title: z.string().describe("The title of the job for which the interview report is generated"),
 })
 
-async function generateInterviewReport({ resume, selfDescription, jobDescription }){
+async function generateInterviewReport({ resume, selfDescription, jobDescription }) {
 
 }
 
