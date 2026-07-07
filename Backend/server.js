f472ed8e10a71fd9afa41bf687913bcc409f0b84
gauranshsahu <gauranshsahu5@gmail.com>
2026-07-08 01:24:58 +0530

    Updated work

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config()
 const app = require('./src/app');
 const connectToDb = require("./src/config/database")
-
+const { resume, selfDescription, jobDescription } = require("./src/services/temp")
+const generateInterviewReport = require("./src/services/ai.service")
 
 connectToDb()
+generateInterviewReport({ resume, selfDescription, jobDescription })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server is running on port 3000");
 })
