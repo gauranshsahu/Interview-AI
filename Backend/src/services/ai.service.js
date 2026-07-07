896e735450c9fd185cf303239ecf47082d0d77a2
gauranshsahu <gauranshsahu5@gmail.com>
2026-07-07 17:03:12 +0530

    updated code

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -17,7 +17,20 @@ const ai = new GoogleGenAI({
 // }
 
 const interviewReportSchema = z.object({
-    technicalQuestions: z.array(z.object({}))
+    technicalQuestions: z.array(z.object({
+        question: z.string().description("The technical question can be asked in the interview"),
+        intention: z.string().description("The intention of interviewer behind asking this question"),
+        answer: z.string().description("How to answer this question, what points to cover, what approach to take etc.")
+    })).description("Behavioral questions that can be asked in the interview along with their intention and how to answer them"),
+    behavioralQuestions: z.array(z.object({
+        question: z.string().description("The technical question can be asked in the interview"),
+        intention: z.string().description("The intention of interviewer behind asking this question"),
+        answer: z.string().description("How to answer this question, what points to cover, what approach to take etc.")
+    })).description("Behavioral questions that can be asked in the interview along with their intention and how to answer them"),
+    skillGaps: z.array(z.object({
+        skill: z.string().description("The skill which the candidate is lacking"),
+        severity: z.enum(["low","medium","high"]).description("The severity of this skill gap, i.e. low,medium and high")
+    }))
 })
 
 async function generateInterviewReport({ resume, selfDescription, jobDescription }){
